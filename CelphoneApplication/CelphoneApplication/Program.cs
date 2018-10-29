c8b5497c81705dd4e26083ff95368ded0d624df0
Juan Carlos <juancho131401@gmail.com>
2018-10-29 17:03:03 -0600

    Creacion y Seleccion de Celulares

--- CelphoneApplication/CelphoneApplication/Program.cs
+++ CelphoneApplication/CelphoneApplication/Program.cs
@@ -14,48 +14,61 @@ namespace CelphoneApplication
             Celulares.Add(new Celular());
             Celulares.Add(new Celular());
             Celulares.Add(new Celular());
-            
-            Console.WriteLine("¿Que celular quieres utilizar\n" +
-                              "1 >> Iphone X\n" +
-                              "2 >> Galaxy s9+\n" +
-                              "3 >> Xiaomi P22\n");
-            int cel = int.Parse(Console.ReadLine());
 
-            Celulares[cel-1].Saldo = 500;
-            Celulares[cel-1].NumeroCel = "5568044458";
-
-            char menu;
-            Console.WriteLine("Selecciona una opcion");
+            int cel;
             do
             {
-                Console.WriteLine("1 >> Llamar.");
-                Console.WriteLine("2 >> Mensajear.");
-                Console.WriteLine("3 >> Ingresar Saldo.");
-                Console.WriteLine("4 >> Checar Saldo.");
-                Console.WriteLine("0 >> Apagar.");
-                menu = char.Parse(Console.ReadLine());
-                switch (menu)
+                Console.WriteLine("¿Que celular quieres utilizar\n" +
+                                  "1 >> Iphone X\n" +
+                                  "2 >> Galaxy s9+\n" +
+                                  "3 >> Xiaomi\n" +
+                                  "0 >> Salir");
+                cel = int.Parse(Console.ReadLine());
+
+                if (cel != 0)
                 {
-                     case '1':
-                         Celulares[cel-1].LLamar();
-                         break;
-                     case '2':
-                         Celulares[cel-1].Mensajear();
-                         break;
-                     case '3':
-                         Celulares[cel-1].Ingresar_saldo();
-                         break;
-                     case '4':
-                         Celulares[cel-1].Checar_saldo();
-                         break;
-                     case '0':
-                         Celulares[cel-1].Apagar();
-                         break;
-                     default:
-                         Console.WriteLine("Ingresa una opcion viable");
-                         break;
+                    Console.Write("Ingresa el numero de tu celular : ");
+                    Celulares[cel - 1].NumeroCel = Console.ReadLine();
+                    Console.Write("Ingresa tu Saldo inicial: $");
+                    Celulares[cel - 1].Saldo = 0;
+                    Celulares[cel - 1].Saldo += int.Parse(Console.ReadLine());
+
+
+                    char menu;
+                    Console.WriteLine("Selecciona una opcion");
+                    do
+                    {
+                        Console.WriteLine("1 >> Llamar.");
+                        Console.WriteLine("2 >> Mensajear.");
+                        Console.WriteLine("3 >> Ingresar Saldo.");
+                        Console.WriteLine("4 >> Checar Saldo.");
+                        Console.WriteLine("0 >> Apagar.");
+                        menu = char.Parse(Console.ReadLine());
+
+                        switch (menu)
+                        {
+                            case '1':
+                                Celulares[cel - 1].LLamar();
+                                break;
+                            case '2':
+                                Celulares[cel - 1].Mensajear();
+                                break;
+                            case '3':
+                                Celulares[cel - 1].Ingresar_saldo();
+                                break;
+                            case '4':
+                                Celulares[cel - 1].Checar_saldo();
+                                break;
+                            case '0':
+                                Celulares[cel - 1].Apagar();
+                                break;
+                            default:
+                                Console.WriteLine("Ingresa una opcion viable");
+                                break;
+                        }
+                    } while (menu != '0');
                 }
-            } while (menu != '0');
+            } while (cel != 0);
         }
     }
 }
